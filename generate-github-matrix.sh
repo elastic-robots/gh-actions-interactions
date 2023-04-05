4bc29c44a35187bde79d128ab3e342884c2e1b40
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-04-05 19:28:13 +0200

    action: parse github command

--- generate-github-matrix.sh
+++ generate-github-matrix.sh
@@ -3,4 +3,6 @@ set -e
 
 echo "versions=$(jq -c .versions matrix.json)" >> $GITHUB_OUTPUT
 echo "modules=$(jq -c .modules matrix.json)" >> $GITHUB_OUTPUT
-echo "pull-request=${PR_NUMBER}" >> $GITHUB_OUTPUT
+
+echo "modules=$(jq -c .modules matrix.json)"
+echo "modules=$MODULES"
